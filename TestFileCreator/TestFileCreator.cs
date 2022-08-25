4df97acadecd9f71f54e6fdb66b3daa017cf5a52
Geert Bellekens <geert@bellekens.com>
2022-08-25 09:45:48 +0200

    initial commit

---
+++ TestFileCreator/TestFileCreator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFileCreator
+{
+    internal class TestFileCreator
+    {
+        public static string createTestFile(string startFile, string destinationFile, long repeats)
+        {
+            var feedback = new StringBuilder();
+            feedback.AppendLine($"{DateTime.Now:dd/MM/yyyy hh:mm:ss.fff} Start creating testfile '{destinationFile}'");
+            long j = 0;
+            using (var destFileWriter = new System.IO.StreamWriter(destinationFile, false))
+            {
+                var atDestinationStart = true;
+                var endline = string.Empty;
+                
+                for (int i = 0; i < repeats; i++)
+                {
+                    using (var startFileReader = System.IO.File.OpenText(startFile))
+                    {
+                        string currentline = null;
+                        string previousLine = null;
+                        var atSourceStart = true;
+                        while ((currentline = startFileReader.ReadLine()) != null)
+                        {
+                            if ( atSourceStart)
+                            {
+                                if (atDestinationStart)
+                                {
+                                    //we are at the very first line, set it up to be written and set flag to false
+                                    previousLine = currentline;
+                                    atDestinationStart = false;
+                                }
+                                atSourceStart = false;
+                            }
+                            else
+                            {
+                                if (previousLine != null)
+                                { 
+                                    //write previous line
+                                    destFileWriter.WriteLine(previousLine);
+                                    j++;
+                                }
+                                //remember this line to be written
+                                previousLine = currentline;
+                                endline = currentline;
+                            }
+                        }
+                    }
+                 }
+                //now write the very last line
+                destFileWriter.WriteLine(endline);
+                j++;
+            }
+            feedback.AppendLine($"{DateTime.Now:dd/MM/yyyy hh:mm:ss.fff} Finished creating testfile '{destinationFile}' with {j} lines");
+            return feedback.ToString();
+        }
+    }
+}
---
+++ TestFileCreator/TestFileCreatorForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+using System.IO;
+
+namespace TestFileCreator
+{
+    public partial class TestFileCreatorForm : Form
+    {
+        
+        public TestFileCreatorForm()
+        {
+            InitializeComponent();
+            enableDisable();
+        }
+        private void enableDisable()
+        {
+            
+        }
+
+        private void browseStartFileButton_Click(object sender, EventArgs e)
+        {
+            //let the user select a file
+            var browseFileDialog = new OpenFileDialog();
+            browseFileDialog.Filter = "XML Files|*.xml";
+            browseFileDialog.FilterIndex = 1;
+            browseFileDialog.Multiselect = false;
+            var dialogResult = browseFileDialog.ShowDialog(this);
+            if (dialogResult == DialogResult.OK)
+            {
+                //if the user selected the file then put the filename in the abbreviationsfileTextBox
+                this.startFileTextBox.Text = browseFileDialog.FileName;
+                //move cursor to end to make sure the end of the path is visible
+                this.startFileTextBox.Select(this.startFileTextBox.Text.Length, 0);
+            }
+        }
+
+        private void createButtonClick(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            this.feedbackTextBox.Clear();
+            var feedback = TestFileCreator.createTestFile(this.startFileTextBox.Text,this.destinationFileTextBox.Text, (long)this.repeatsUpDown.Value);
+            this.feedbackTextBox.Text = string.IsNullOrEmpty(feedback) ? "Finished without feedback" : feedback;
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void browseDestinationFileButton_Click(object sender, EventArgs e)
+        {
+            //let the user select a file
+            var browseFileDialog = new SaveFileDialog();
+            browseFileDialog.Filter = "XML Files|*.xml";
+            browseFileDialog.FilterIndex = 1;
+            var dialogResult = browseFileDialog.ShowDialog(this);
+            if (dialogResult == DialogResult.OK)
+            {
+                //if the user selected the file then put the filename in the abbreviationsfileTextBox
+                this.destinationFileTextBox.Text = browseFileDialog.FileName;
+                //move cursor to end to make sure the end of the path is visible
+                this.destinationFileTextBox.Select(this.destinationFileTextBox.Text.Length, 0);
+            }
+        }
+    }
+}
